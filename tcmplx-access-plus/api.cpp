51c1797a8c2903835703c2055a33ebf9889144d6
Cody Licorish <svgmovement@gmail.com>
2020-01-16 19:10:00 -0600

    Repair names

--- tcmplx-access-plus/api.cpp
+++ tcmplx-access-plus/api.cpp
@@ -1,15 +1,17 @@
 /*
- * \file tcmplx/api.cpp
+ * \file tcmplx-access-plus/api.cpp
  * \brief API-wide declarations for text-complex
  * \author Cody Licorish (svgmovement@gmail.com)
  */
-#define TCMPLX_PLUS_WIN32_DLL_INTERNAL
+#define TCMPLX_AP_WIN32_DLL_INTERNAL
 #include "api.hpp"
 
 namespace text_complex {
-  //BEGIN configurations
-  char const* api_version(void) noexcept {
-    return "0.0";
-  }
-  //END   configurations
+  namespace access {
+    //BEGIN configurations
+    char const* api_version(void) noexcept {
+      return "0.0";
+    }
+    //END   configurations
+  };
 };
--- tcmplx-access-plus/api.hpp
+++ tcmplx-access-plus/api.hpp
@@ -1,30 +1,36 @@
 /*
- * \file tcmplx-plus/api.hpp
+ * \file tcmplx-access-plus/api.hpp
  * \brief API-wide declarations for text-complex-plus
  * \author Cody Licorish (svgmovement@gmail.com)
  */
-#ifndef hg_TextComplex_Plus_api_H_
-#define hg_TextComplex_Plus_api_H_
+#ifndef hg_TextComplexAccessP_api_H_
+#define hg_TextComplexAccessP_api_H_
 
-#ifdef TCMPLX_PLUS_WIN32_DLL
-#  ifdef TCMPLX_PLUS_WIN32_DLL_INTERNAL
-#    define TCMPLX_PLUS_API __declspec(dllexport)
+#ifdef TCMPLX_AP_WIN32_DLL
+#  ifdef TCMPLX_AP_WIN32_DLL_INTERNAL
+#    define TCMPLX_AP_API __declspec(dllexport)
 #  else
-#    define TCMPLX_PLUS_API __declspec(dllimport)
-#  endif /*TCMPLX_PLUS_WIN32_DLL_INTERNAL*/
+#    define TCMPLX_AP_API __declspec(dllimport)
+#  endif /*TCMPLX_AP_WIN32_DLL_INTERNAL*/
 #else
-#  define TCMPLX_PLUS_API
-#endif /*TCMPLX_PLUS_WIN32_DLL*/
+#  define TCMPLX_AP_API
+#endif /*TCMPLX_AP_WIN32_DLL*/
 
 /**
  * \brief Text processing library
  */
 namespace text_complex {
   /**
-   * \brief Check the library's version.
-   * \return a version string
+   * \brief Data access
    */
-  char const* api_version(void) noexcept;
+  namespace access {
+    /**
+     * \brief Check the library's version.
+     * \return a version string
+     */
+    TCMPLX_AP_API
+    char const* api_version(void) noexcept;
+  };
 };
 
-#endif //hg_TextComplex_Plus_api_H_
+#endif //hg_TextComplexAccessP_api_H_
